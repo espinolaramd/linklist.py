442be5872e87b6cfbe8ec01048a8d984e30097ea
espinolaramd <espinolaramd@hartwick.edu>
2020-02-23 21:33:04 -0500

    update

---
+++ driver.py
@@ -0,0 +1,4 @@
+from linklist import linked_list
+
+myList = linked_list()
+
--- linklist.py
+++ linklist.py
@@ -1,3 +1,24 @@
 #Diego Espinola
 #02.19.20
 #create a link list and add to the head.
+
+class   Node:
+
+    def __init__(self,data):
+        self.data = data
+        self.nextNode = None
+
+class linked_list:
+
+    def __init__(self):
+        self.head = None
+
+
+    def add_head(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+
+
+
